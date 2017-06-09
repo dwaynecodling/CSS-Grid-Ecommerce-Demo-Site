e20d2e10016b85fcade81ad4d8d6dcb78004cc64
dwaynecodling <dwayne@wearejh.com>
2017-06-09 14:57:50 +0100

    changes files to support crossbow.yml and sequencing task, with browsersync.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 const path = require('path');
 
 module.exports = {
-    entry: "./app/assets/scripts/App.js",
+    context: path.join(__dirname,'app','assets'),
+    entry: {
+        app: './scripts/App.js'
+},
     output: {
-        path: path.resolve(__dirname, 'app/temp/scripts'),
-        filename: "App.js"
+        path: path.join(__dirname, 'app', 'temp', 'scripts'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: path.join(__dirname,'app','assets'),
+                query: {
+                    presets: ['es2015']
+                },
+            }
+        ]
     }
-}
+};
 
