3cc42b35e987cd29965952736a24396d361056a9
dwaynecodling <dwayne@wearejh.com>
2017-06-16 17:01:47 +0100

    Adding product list grid on click

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,63 @@
 var $ = require('jquery');
 var Person = require('./modules/Person');
 
-var dwayne = new Person("Dwayne Codling", "purple");
+var dwayne = new Person("Dwayne", "purple");
 dwayne.greet();
 
-var kelly = new Person("Kelly Edwards", "onyx");
+var kelly = new Person("Danielle", "onyx");
 kelly.greet();
 
 [...document.querySelectorAll('div[data-image]')].forEach((element) => {
     var image=element.dataset.image;
     element.style['backgroundImage'] = `url("${image}")`;
-})
+});
+
+//all items
+var item = $('.product__filter-item');
+
+item.each(function($i) {
+    var $this = $(this);
+
+    $this.on('click', function(e){
+
+        item
+            .not($this)
+            .removeClass('active');
+
+        var state = $this.hasClass('active');
+
+        if(state) {
+            return;
+        } else  {
+            $this.addClass('active');
+
+            $('.products')
+                .attr('class', 'products')
+                .addClass('grid-' + $i);
+        }
+
+    });
+});
+
+
+
+// var item = $('.product__filter-item a');
+//
+// item.each(function($i) {
+//     var $this = $(this);
+//
+//     $this.hover(function(){
+//         $this.attr('href');
+//         const hashValue = $this.attr('href').replace(/$#/,'');
+//         const currentUrl = new URL(location.href);
+//         currentUrl.hash = hashValue;
+//         location.replace(currentUrl);
+//
+//             $this.toggleClass('active');
+//             $('.products').toggleClass('grid-' + $i);
+//
+//
+//     });
+// });
+//
+
--- app/temp/scripts/app.bundle.js
+++ app/temp/scripts/app.bundle.js
@@ -10355,17 +10355,58 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 var $ = __webpack_require__(1);
 var Person = __webpack_require__(0);
 
-var dwayne = new Person("Dwayne Codling", "purple");
+var dwayne = new Person("Dwayne", "purple");
 dwayne.greet();
 
-var kelly = new Person("Kelly Edwards", "onyx");
+var kelly = new Person("Danielle", "onyx");
 kelly.greet();
 
 [].concat(_toConsumableArray(document.querySelectorAll('div[data-image]'))).forEach(function (element) {
     var image = element.dataset.image;
-    console.log({ element: element });
     element.style['backgroundImage'] = 'url("' + image + '")';
 });
+
+//all items
+var item = $('.product__filter-item');
+
+item.each(function ($i) {
+    var $this = $(this);
+
+    $this.on('click', function (e) {
+
+        item.not($this).removeClass('active');
+
+        var state = $this.hasClass('active');
+
+        if (state) {
+            return;
+        } else {
+            $this.addClass('active');
+
+            $('.products').attr('class', 'products').addClass('grid-' + $i);
+        }
+    });
+});
+
+// var item = $('.product__filter-item a');
+//
+// item.each(function($i) {
+//     var $this = $(this);
+//
+//     $this.hover(function(){
+//         $this.attr('href');
+//         const hashValue = $this.attr('href').replace(/$#/,'');
+//         const currentUrl = new URL(location.href);
+//         currentUrl.hash = hashValue;
+//         location.replace(currentUrl);
+//
+//             $this.toggleClass('active');
+//             $('.products').toggleClass('grid-' + $i);
+//
+//
+//     });
+// });
+//
 
 /***/ })
 /******/ ]);
